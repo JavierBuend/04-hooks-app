c9c490c36c81cd49afef5f4872a52d570ed8e294
Javier <186595036+cepex1@users.noreply.github.com>
2026-03-13 12:03:44 +0100

    useRef para no disparar el re-render

--- src/04-useRef/FocusScreen.tsx
+++ src/04-useRef/FocusScreen.tsx
@@ -1,3 +1,28 @@
+import { useRef } from "react";
+
 export const FocusScreen = () => {
-  return <div>FocusScreen</div>;
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    console.log(inputRef.current?.value);
+    inputRef.current?.select();
+  };
+
+  return (
+    <div className="bg-gradient flex flex-col gap-4">
+      <h1 className="text-2xl fonth-thin text-white">Focus screen</h1>
+      <input
+        ref={inputRef}
+        type="text"
+        className="bg-white text-black px-4 py-2 rounded-md"
+        autoFocus
+      ></input>
+      <button
+        className="bg-blue-500 text-white px-4 py-2 rounded-md cursor-pointer"
+        onClick={handleClick}
+      >
+        Set focus
+      </button>
+    </div>
+  );
 };
