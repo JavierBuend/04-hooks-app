391cb5dd8f538a513f553288e8e361f26b108574
Javier <186595036+cepex1@users.noreply.github.com>
2026-03-18 12:27:52 +0100

    Shadcn instalación

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,9 @@ import { createRoot } from "react-dom/client";
 
 import "./index.css";
 // import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook";
-import { PokemonPage } from "./03-examples/PokemonPage";
+// import { PokemonPage } from "./03-examples/PokemonPage";
 import { FocusScreen } from "./04-useRef/FocusScreen";
+import { TasksApp } from "./05-useReducer/TaskApp";
 // import { TrafficLight } from "./01-useState/TrafficLight";
 
 createRoot(document.getElementById("root")!).render(
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
     {/* <TrafficLight /> */}
     {/* <TrafficLightWithHook /> */}
     {/* <PokemonPage /> */}
-    <FocusScreen />
+    {/* {<FocusScreen />} */}
+    <TasksApp />
   </StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import path from "path"
+import { fileURLToPath } from "url"
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
+import tailwindcss from "@tailwindcss/vite"
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve:{
-    alias:{
-      "@":path.resolve(__dirname,"./src")
-    }
-  }
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
 })
