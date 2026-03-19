edb27eb40ad9bbe39cf19634f7c5237de2ffca89
Javier <186595036+cepex1@users.noreply.github.com>
2026-03-19 11:49:05 +0100

    useState en todo list

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 // import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook";
 // import { PokemonPage } from "./03-examples/PokemonPage";
-import { FocusScreen } from "./04-useRef/FocusScreen";
+// import { FocusScreen } from "./04-useRef/FocusScreen";
 import { TasksApp } from "./05-useReducer/TaskApp";
 // import { TrafficLight } from "./01-useState/TrafficLight";
 
