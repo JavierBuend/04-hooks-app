f7447bced9d8d147f06d0d0627b79a9d9edca3e8
Javier <186595036+cepex1@users.noreply.github.com>
2026-04-09 12:21:51 +0200

    ScrambleWords

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import "./index.css";
 // import { PokemonPage } from "./03-examples/PokemonPage";
 // import { FocusScreen } from "./04-useRef/FocusScreen";
 import { TasksApp } from "./05-useReducer/TaskApp";
+import { ScrambleWords } from "./05-useReducer/reducer/ScrambleWords";
 // import { TrafficLight } from "./01-useState/TrafficLight";
 
 createRoot(document.getElementById("root")!).render(
@@ -14,6 +15,7 @@ createRoot(document.getElementById("root")!).render(
     {/* <TrafficLightWithHook /> */}
     {/* <PokemonPage /> */}
     {/* {<FocusScreen />} */}
-    <TasksApp />
+    {/* <TasksApp /> */}
+    <ScrambleWords />
   </StrictMode>,
 );
